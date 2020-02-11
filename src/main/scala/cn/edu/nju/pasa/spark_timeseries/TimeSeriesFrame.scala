ad4dad75a9ab34497aed63ee2a77e94de03ae4f4
HazzaCheng <hazzacheng@gmail.com>
2020-02-11 14:45:01 +0800

    finish TimeSeriesFrame

--- src/main/scala/cn/edu/nju/pasa/spark_timeseries/TimeSeriesFrame.scala
+++ src/main/scala/cn/edu/nju/pasa/spark_timeseries/TimeSeriesFrame.scala
@@ -1,5 +1,37 @@
 package cn.edu.nju.pasa.spark_timeseries
 
-class TimeSeriesFrame {
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.storage.StorageLevel
 
+class TimeSeriesFrame(val rawDataDF: DataFrame, val ss: SparkSession) {
+  private val timeSeriesDF = TimeSeriesFrame.getTimeSeriesDF(rawDataDF, ss)
+
+  def getTimeSeriesDF(): DataFrame = {
+    timeSeriesDF
+  }
+
+  def getTimeSeriesByFeatureName(name: String): Vector = {
+    val vec = timeSeriesDF.where("feature = '" + name + "'")
+      .select("vector").collect()
+      .last.getAs[Vector](0)
+
+    vec
+  }
+}
+
+object TimeSeriesFrame {
+  def getTimeSeriesDF(rawDataDF: DataFrame, ss:SparkSession): DataFrame = {
+    val timeseriesRdd = rawDataDF.rdd
+      .map(x => (x.getString(1), List((x.getLong(0), x.getDouble(2)))))
+      .reduceByKey((x, y) => x ++ y)
+      .map(x => (x._1, x._2.sortBy(_._1)))
+      .map(x => (x._1, Vectors.dense(x._2.map(_._2).toArray)))
+
+    val timeseriesDF = ss.createDataFrame(timeseriesRdd).toDF("feature", "vector")
+        .persist(StorageLevel.MEMORY_AND_DISK_SER)
+
+    timeseriesDF
+  }
 }
--- src/test/scala/cn/edu/nju/pasa/spark_timeseries/TestTimeSeriesFrame.scala
+++ src/test/scala/cn/edu/nju/pasa/spark_timeseries/TestTimeSeriesFrame.scala
@@ -1,23 +1,45 @@
 package cn.edu.nju.pasa.spark_timeseries
 
+import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{FloatType, IntegerType, StringType, StructField}
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
 class TestTimeSeriesFrame extends FunSuite with BeforeAndAfterAll {
-  private var spark:SparkSession = _
+  private var ss:SparkSession = _
+
 
   override def beforeAll(): Unit = {
-    spark = SparkSession.builder().appName("udf testings")
+    ss = SparkSession.builder().appName("local_test")
       .master("local")
       .config("", "")
       .getOrCreate()
   }
 
   override def afterAll(): Unit = {
-    spark.close()
+    ss.close()
   }
 
   test("test TimeSeriesFrame") {
+    val data = Array(
+      (0L, "A", 1.2),
+      (3L, "B", 3.7),
+      (3L, "A", 4.2),
+      (1L, "C", 1.4),
+      (2L, "A", 12.2),
+      (0L, "C", 1.9),
+      (1L, "A", 1.2),
+      (0L, "B", 2.7),
+      (2L, "B", 3.6),
+      (3L, "C", 1.2),
+      (1L, "B", 2.2),
+      (2L, "C", 2.2))
 
+    val rawDataDF = ss.createDataFrame(data).toDF("timestamp", "key", "value")
+
+    val tsf = new TimeSeriesFrame(rawDataDF, ss)
+
+    assert(tsf.getTimeSeriesDF().count() == 3)
+    assert(tsf.getTimeSeriesByFeatureName("A") == Vectors.dense(1.2, 1.2, 12.2, 4.2))
   }
 }
