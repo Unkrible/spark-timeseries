8c7366b428462f68e7896c2acea1e5b91c9bcbb9
rex <xuzhuoer.rex@gmail.com>
2020-02-16 21:12:52 +0800

    [test] add local main test

--- src/main/scala/cn/edu/nju/pasa/spark_timeseries/Main.scala
+++ src/main/scala/cn/edu/nju/pasa/spark_timeseries/Main.scala
@@ -1,5 +1,32 @@
 package cn.edu.nju.pasa.spark_timeseries
 
-object Main {
+import org.apache.spark.sql.SparkSession
 
+object Main extends App {
+  override def main(args: Array[String]): Unit = {
+    val ss = SparkSession
+      .builder()
+      .appName("Time Series")
+      .master("local")
+      .getOrCreate()
+    val df = ss.createDataFrame(Seq(
+      (5L, "GOOG", 523.0),
+      (6L, "GOOG", -1.0),
+      (7L, "GOOG", 524.0),
+      (8L, "GOOG", 600.0),
+      (5L, "AAPL", 384.0),
+      (6L, "AAPL", 384.0),
+      (7L, "AAPL", 385.0),
+      (8L, "AAPL", 385.0),
+      (5L, "MSFT", 40.0),
+      (6L, "MSFT", 60.0),
+      (7L, "MSFT", -1.0),
+      (8L, "MSFT", 70.0)
+    )).toDF("Timestamp", "Key", "Value")
+    df.show()
+    val tsFrame = new TimeSeriesFrame(df, ss)
+    tsFrame.getTimeSeriesDF.show
+    val autoCross = new AutoCross(ss, tsFrame)
+    autoCross.getFeatureSet(3)
+  }
 }
