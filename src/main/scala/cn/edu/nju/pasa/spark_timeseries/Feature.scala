e7b76fdd84a329df41439222dd80b830e2120f4e
rex <xuzhuoer.rex@gmail.com>
2020-02-29 02:03:28 +0800

    [feat] translate Feature to String & translate String to Feature & parse Feature name to ops

--- src/main/scala/cn/edu/nju/pasa/spark_timeseries/Feature.scala
+++ src/main/scala/cn/edu/nju/pasa/spark_timeseries/Feature.scala
@@ -1,13 +1,27 @@
 package cn.edu.nju.pasa.spark_timeseries
 
-class Feature(val feature: String, val ops: List[String] = List.empty[String]) {
+class Feature(val feature: String) {
+  val (name, ops) = Feature.parseFeat(feature)
   /***
     * Generate new feature name by ops
     * @return new feature name
     */
+  def this(name: String, ops: List[String]) {
+    this(Feature.getFeatName(name, ops))
+  }
+
   override def toString: String = {
     // feature name format
     // e.g. A$sum#roll#diff means feature A uses op sum and op diff
-    feature + ops.mkString(if (ops.nonEmpty) "$" else "", "#", "")
+    feature
+  }
+}
+
+object Feature {
+  def getFeatName(name: String, ops: List[String]): String = name + ops.mkString(if (ops.nonEmpty) "$" else "", "#", "")
+
+  def parseFeat(feat: String): (String, List[String]) = {
+    val info = feat.split("\\$")
+    (info.head, info.tail.toList)
   }
 }
