e3877beaa57e29444f61da139b8ccce664e6ccce
rex <xuzhuoer.rex@gmail.com>
2020-03-01 14:18:32 +0800

    [feat] add conf

--- src/main/scala/cn/edu/nju/pasa/spark_timeseries/Main.scala
+++ src/main/scala/cn/edu/nju/pasa/spark_timeseries/Main.scala
@@ -1,32 +1,29 @@
 package cn.edu.nju.pasa.spark_timeseries
 
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
 object Main extends App {
   override def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+      .set("spark.default.parallelism", "500")
+      .set("spark.speculation", "true")
+      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     val ss = SparkSession
       .builder()
       .appName("Time Series")
-      .master("local")
+//      .master("local")
+      .config(conf)
       .getOrCreate()
-    val df = ss.createDataFrame(Seq(
-      (5L, "GOOG", 523.0, math.random),
-      (6L, "GOOG", -1.0, math.random),
-      (7L, "GOOG", 524.0, math.random),
-      (8L, "GOOG", 600.0, math.random),
-      (5L, "AAPL", 384.0, math.random),
-      (6L, "AAPL", 384.0, math.random),
-      (7L, "AAPL", 385.0, math.random),
-      (8L, "AAPL", 385.0, math.random),
-      (5L, "MSFT", 40.0, math.random),
-      (6L, "MSFT", 60.0, math.random),
-      (7L, "MSFT", -1.0, math.random),
-      (8L, "MSFT", 70.0, math.random)
-    )).toDF("timestamp", "key", "value", "label")
+    val dataset =
+      (0 until args(0).toInt).map(i => (i.toLong, "GOOG", math.random, math.random)) ++
+        (0 until args(0).toInt).map(i => (i.toLong, "AAPL", math.random, math.random)) ++
+        (0 until args(0).toInt).map(i => (i.toLong, "MSFT", math.random, math.random))
+    val df = ss.createDataFrame(dataset).toDF("timestamp", "key", "value", "label")
     df.show()
     val tsFrame = new TimeSeriesFrame(df, ss)
     tsFrame.getTimeSeriesDF.show
     val autoCross = new AutoCross(ss, tsFrame)
-    autoCross.searchFeature(3)
+    autoCross.searchFeature(args(1).toInt)
   }
 }
